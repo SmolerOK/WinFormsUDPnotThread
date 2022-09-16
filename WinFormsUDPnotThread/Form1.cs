a3a1d128e956ce809de2d8b785687dffa9453866
Sergei <Sergei@DESKTOP-AH90HTM>
2022-09-16 17:24:20 +0300

    Добавьте файлы проекта.

---
+++ WinFormsUDPnotThread/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace WinFormsUDPnotThread
+{
+    public partial class Form1 : Form
+    {
+        private byte[] dataOpenDoor0 = { 0x02, 0x1F, 0x00, 0x03, 0x61, 0x38 };
+        private byte[] dataOpenDoor1 = { 0x02, 0x1F, 0x01, 0x03, 0xB9, 0x21 };
+        private UdpClient client;
+        private Ping ping;
+        private PingReply pingReply;
+
+        public Form1()
+        {
+            InitializeComponent();
+            client = new UdpClient();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendMessage(dataOpenDoor0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SendMessage(dataOpenDoor1);
+        }
+
+        private void SendMessage(byte[] message)
+        {
+            try
+            {
+                ping = new Ping();
+                IPAddress address = IPAddress.Parse(textBox1.Text);
+                pingReply = ping.Send(address);
+                client.Connect(address, 8192);
+
+                client.Send(dataOpenDoor0, dataOpenDoor0.Length);
+
+                IPEndPoint iPEndPoint = new IPEndPoint(address, 8192);
+
+                if (pingReply.Status != IPStatus.Success)
+                    throw new Exception("IP не найден!");
+
+                byte[] receiveByte = client.Receive(ref iPEndPoint);
+
+                string hex = BitConverter.ToString(receiveByte, 0, receiveByte.Length);
+                label2.Text = DateTime.Now + " -> " + hex;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
